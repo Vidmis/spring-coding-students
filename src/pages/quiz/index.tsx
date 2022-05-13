c029bf53816df556243bce38f92c8d4fa21e5c39
Vidmis <v.gutauskas90@gmail.com>
2022-05-13 17:59:09 +0300

    changing branch

---
+++ src/pages/quiz/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ContentWrapper } from "components";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <ContentWrapper maxWidth='100%'>
+        {/* <QuizBox></QuizBox> */}
+      </ContentWrapper>
+    </>
+  );
+};
+
+export default Home;
